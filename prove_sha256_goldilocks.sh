d70839e801fdbc38a97ea393de1646a5836c3e2f
Lukas <lucidLuckylee@users.noreply.github.com>
2023-07-18 17:01:05 +0200

    add goldilocks script

---
+++ prove_sha256_goldilocks.sh
@@ -0,0 +1,23 @@
+cd sha256_cairo_goldilocks
+TARGET="goldilocks_target"
+mkdir ${TARGET}
+
+# Compile the cairo program
+cairo-compile main.cairo --output=${TARGET}/main_compiled.json --proof_mode --prime 18446744069414584321
+# Start the runner to generate the air inputs
+cairo-run --program=${TARGET}/main_compiled.json \
+        --layout=all --print_output \
+        --trace_file ${TARGET}/trace.bin \
+        --memory_file ${TARGET}/memory.bin \
+        --air_private_input ${TARGET}/air-private-input.json \
+        --air_public_input ${TARGET}/air-public-input.json \
+        --min_steps 128 --proof_mode --print_info
+cp -r ${TARGET} ../sandstorm-mirror/${TARGET}
+cd ../sandstorm-mirror
+
+\time -f "%E %M" cargo +nightly run -r -F parallel,asm -- \
+ --program ${TARGET}/main_compiled.json \
+ --air-public-input ${TARGET}/air-public-input.json \
+ prove \
+ --air-private-input ${TARGET}/air-private-input.json \
+ --output ${TARGET}/sha256_goldilocks_main.proof
